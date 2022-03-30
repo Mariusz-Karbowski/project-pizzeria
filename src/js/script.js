42fc4451d68674cc45ab9737340c8650cbaa5487
Mariusz-Karbowski <m.karbowski@outlook.com>
2022-03-30 16:36:36 +0200

    Product accordion added

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,60 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(id, data){
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+      
+      console.log('new Product', thisProduct);
+    }
+
+    renderInMenu(){
+      const thisProduct = this;
+
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion(){
+      const thisProduct = this;
+
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      clickableTrigger.addEventListener('click', function(event){
+        event.preventDefault();
+        const activeProducts = document.querySelectorAll(select.all.menuProducts);
+        for(let activeProduct of activeProducts){
+          if(activeProduct != thisProduct.element){
+            activeProduct.classList.remove('active');
+          } else thisProduct.element.classList.toggle('active');
+        }
+      });
+    }
+  }
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+    
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +113,9 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
