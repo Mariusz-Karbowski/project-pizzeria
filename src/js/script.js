52fa190cb4aa0395146584e57f92dcff396cafa0
Mariusz-Karbowski <m.karbowski@outlook.com>
2022-04-01 16:11:32 +0200

    Added option price calculator

--- src/js/script.js
+++ src/js/script.js
@@ -55,30 +55,35 @@
   class Product{
     constructor(id, data){
       const thisProduct = this;
-
       thisProduct.id = id;
       thisProduct.data = data;
-
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
-      
-      console.log('new Product', thisProduct);
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
     }
 
     renderInMenu(){
       const thisProduct = this;
-
       const generatedHTML = templates.menuProduct(thisProduct.data);
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
       const menuContainer = document.querySelector(select.containerOf.menu);
       menuContainer.appendChild(thisProduct.element);
     }
 
-    initAccordion(){
+    getElements(){
       const thisProduct = this;
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
 
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-      clickableTrigger.addEventListener('click', function(event){
+    initAccordion(){
+      const thisProduct = this;      
+      thisProduct.accordionTrigger.addEventListener('click', function(event){
         event.preventDefault();
         const activeProducts = document.querySelectorAll(select.all.menuProducts);
         for(let activeProduct of activeProducts){
@@ -88,24 +93,56 @@
         }
       });
     }
+
+    initOrderForm(){
+      const thisProduct = this;      
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder(){
+      const thisProduct = this;
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      let price = thisProduct.data.price;
+      for(let paramId in thisProduct.data.params){
+        const param = thisProduct.data.params[paramId];
+        for(let optionId in param.options){
+          const option = param.options[optionId];
+          if(formData[paramId] && formData[paramId].includes(optionId)){
+            if(!option.default){
+              price +=  option.price;
+            }  
+          } else if(option.default){
+            price -= option.price;
+          }
+        }
+      }
+      thisProduct.priceElem.innerHTML = price;
+    }
   }
 
   const app = {
     initMenu: function(){
-      const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
-
+      const thisApp = this;      
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
       }
     },
-
     initData: function(){
       const thisApp = this;
-
       thisApp.data = dataSource;
-    },
-    
+    },    
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -113,7 +150,6 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
-
       thisApp.initData();
       thisApp.initMenu();
     },
